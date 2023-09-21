516ebeb676f88d5d655c5907ed5860bd129c9d2f
SolomonV01 <113135388+SolomonV01@users.noreply.github.com>
2023-09-20 21:50:17 -0500

    cssedit1

--- Week2/main.js
+++ Week2/main.js
@@ -1,24 +1,25 @@
-function sumNum()
+function sumNumSquareNum()
 {
-    let numberOne, numberTwo, sum;
+    let numberOne, numberTwo, sum, squareRootOne, squareRootTwo;
 
     numberOne = parseInt(document.getElementById("numberOne").value);
     numberTwo = parseInt(document.getElementById("numberTwo").value);
 
     sum = numberOne + numberTwo;
     document.getElementById("sumAnswer").value = sum;
-}
-
-function squareNum()
-{
-    let numberOne, numberTwo, squareRootOne, squareRootTwo;
-
-    numberOne = parseInt(document.getElementById("numberOne").value);
-    numberTwo = parseInt(document.getElementById("numberTwo").value);
 
     squareRootOne = Math.sqrt(numberOne);
     document.getElementById("sqrtAnswerOne").value = squareRootOne;
 
     squareRootTwo = Math.sqrt(numberTwo);
     document.getElementById("sqrtAnswerTwo").value = squareRootTwo;
+}
+
+function clearAll()
+{
+    document.getElementById("numberOne").value = "";
+    document.getElementById("numberTwo").value = "";
+    document.getElementById("sumAnswer").value = "";
+    document.getElementById("sqrtAnswerOne").value = "";
+    document.getElementById("sqrtAnswerTwo").value = "";
 }
